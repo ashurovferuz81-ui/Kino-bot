55bd88e6283f607c9ecce370d2842cf141730687
ashurovferuz81-ui <ashurovferuz81@gmail.com>
2026-02-09 11:40:13 +0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// Telegram Bot Token
+define('BOT_TOKEN', '8426836407:AAHoXkQakddqyXZ_olNplG0_ov-3fhvrkSc');
+
+// Admin ID
+define('ADMIN_ID', 5775388579);
+
+// Database sozlamalari
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'kino_bot');
+define('DB_USER', 'root');
+define('DB_PASS', 'password');
+?>
